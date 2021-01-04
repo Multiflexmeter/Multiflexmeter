da4bb711009059f74317aae2153e17b4e7cba62e
Tim van Osch <timvosch@pollex.nl>
2021-01-04 13:23:30 +0100

    Improve sleep accuracy & ADC OFF

--- src/sleep.cpp
+++ src/sleep.cpp
@@ -18,6 +18,7 @@ void sleep(uint32_t ms)
     next_tx = now;
   }
   uint32_t sleep_time_ms = max(ms, next_tx - now);
+  uint32_t time_remaining = sleep_time_ms;
 
   _debug(F("Sleeping for: "));
   _debug(sleep_time_ms / 1000);
@@ -26,18 +27,34 @@ void sleep(uint32_t ms)
   Serial.flush();
 #endif
 
-  while (sleep_time_ms > 0)
+  while (time_remaining > 0)
   {
-    // For now sleeping happens per 8 seconds (8000 ms)
-    uint32_t cycle_ms = 8000;
-    // ADC will actually stay in current state and not turn on
-    LowPower.powerDown(SLEEP_8S, ADC_ON, BOD_OFF);
-
-    // Lower duration
-    if (sleep_time_ms <= cycle_ms)
-      sleep_time_ms = 0;
+    uint32_t slept;
+    if (time_remaining >= 8000)
+    {
+      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+      slept = 8000;
+    }
+    else if (time_remaining >= 4000)
+    {
+      LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+      slept = 4000;
+    }
+    else if (time_remaining >= 2000)
+    {
+      LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
+      slept = 2000;
+    }
+    else if (time_remaining >= 1000)
+    {
+      LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+      slept = 1000;
+    }
     else
-      sleep_time_ms -= cycle_ms;
+    {
+      slept = time_remaining;
+    }
+    time_remaining -= slept;
 
     // Fix timing
     ATOMIC_BLOCK(ATOMIC_FORCEON)
@@ -46,9 +63,10 @@ void sleep(uint32_t ms)
       extern volatile unsigned long timer0_millis;
       // timer0 overflows every 64 * 256 clock cycles
       // https://github.com/arduino/ArduinoCore-avr/blob/6ec80154cd2ca52bce443afbbed8a1ad44d049cb/cores/arduino/wiring.c#L27
-      timer0_overflow_count += microsecondsToClockCycles(cycle_ms * 1000) / (64 * 256);
-      timer0_millis += cycle_ms;
+      timer0_overflow_count += microsecondsToClockCycles(slept * 1000) / (64 * 256);
+      timer0_millis += slept;
     }
   }
+
   _debug(F("Woke up\r\n"));
 }
