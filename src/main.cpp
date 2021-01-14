3fe89207f34b7a288f1d377202ab0e3c9feec89e
Tim van Osch <timvosch@pollex.nl>
2021-01-14 17:19:57 +0100

    Debug unsigned instead of signed, remove ostick2ms round

--- src/main.cpp
+++ src/main.cpp
@@ -111,7 +111,7 @@ void job_measure_and_send(osjob_t *job)
   }
 
   // Schedule our next measurement and send
-  ostime_t next_send = ms2osticksRound(conf_getSleepInterval());
+  ostime_t next_send = ms2osticks(conf_getSleepInterval());
 
   ostime_t now = os_getTime();
   ostime_t next_tx_time = ((LMIC.opmode & OP_NEXTCHNL) != 0) ? LMICbandplan_nextTx(now) : LMIC.txend;
@@ -123,7 +123,7 @@ void job_measure_and_send(osjob_t *job)
 
   _debugTime();
   _debug("Next measure at: ");
-  _debug(osticks2ms(now + next_send));
+  _debug((uint32_t)(now + next_send));
   _debug("\n");
   os_setTimedCallback(job, now + next_send, job_measure_and_send);
 }
