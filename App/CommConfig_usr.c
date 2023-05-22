6c067864e44ed67fce6347b598c688473d2a6098
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-05-22 15:53:05 +0200

    change: add new file commConfig_usr.c

---
+++ App/CommConfig_usr.c
@@ -0,0 +1,13 @@
+/**
+  ******************************************************************************
+  * @addtogroup     : App
+  * @{
+  * @file           : CommConfig_usr
+  * @brief          : Communication configuration user functions
+  * @author         : P.Kwekkeboom
+  * @date           : May 22, 2023
+  * @copyright      : 2023 Dekimo Goes
+  * @}
+  ******************************************************************************
+  */
+
---
+++ App/CommConfig_usr.h
@@ -0,0 +1,15 @@
+/**
+  ******************************************************************************
+  * @file           : CommConfig_usr
+  * @brief          : Header for CommConfig_usr file.
+  * @author         : P.Kwekkeboom
+  * @date           : May 22, 2023
+  * @copyright      : 2023 Dekimo Goes
+  ******************************************************************************
+  */
+#ifndef COMMCONFIG_USR_H_
+#define COMMCONFIG_USR_H_
+
+
+
+#endif /* COMMCONFIG_USR_H_ */
