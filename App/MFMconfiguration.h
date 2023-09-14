fb5a4511f596c0cfca67ef3a249429d4952e857f
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-09-14 08:14:35 +0200

    change: add new files MFMconfiguration.c and MFMconfiguration.h to project.

---
+++ App/MFMconfiguration.h
@@ -0,0 +1,14 @@
+/**
+  ******************************************************************************
+  * @file           : MFMconfiguration.h
+  * @brief          : Header for MFMconfiguration.c file.
+  * @author         : P.Kwekkeboom
+  * @date           : Sep 14, 2023
+  ******************************************************************************
+  */
+#ifndef MFMCONFIGURATION_H_
+#define MFMCONFIGURATION_H_
+
+
+
+#endif /* MFMCONFIGURATION_H_ */
