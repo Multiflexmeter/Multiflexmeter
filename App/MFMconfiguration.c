500b7453b51cc8b25fdddf5df0370677f3bc54a8
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-09-14 08:21:54 +0200

    change: move functions configuration functions to MFMconfiguration.c

--- App/MFMconfiguration.c
+++ App/MFMconfiguration.c
@@ -10,3 +10,165 @@
   ******************************************************************************
   */
 
+#include "platform.h"
+#include "MFMconfiguration.h"
+
+static struct_MFMSettings MFM_settings;
+
+/**
+ * @fn int getSensorStatus(int32_t)
+ * @brief override function getSensorStatus(), can be override in application code
+ *
+ * @param sensorId : 1 to /ref NR_OF_SLOTS
+ * @return -1 = sensorId out of range
+ */
+const int32_t getSensorStatus(int32_t sensorId)
+{
+  assert_param(sensorId >= 1 && sensorId <= NR_OF_SLOTS );
+
+  if( sensorId < 1 || sensorId > NR_OF_SLOTS ) //Verify argument
+  {
+    return -1;
+  }
+
+  return (int32_t)MFM_settings.slotModuleSettings[ sensorId - 1 ].enabled;
+}
+
+/**
+ * @fn int32_t setSensorStatus(int32_t, bool)
+ * @brief override function setSensorStatus(), can be override in application code
+ *
+ * @param sensorId
+ * @param status
+ * @return
+ */
+const int32_t setSensorStatus(int32_t sensorId, bool status)
+{
+  assert_param(sensorId >= 1 && sensorId <= NR_OF_SLOTS );
+
+  if( sensorId < 1 || sensorId > NR_OF_SLOTS ) //Verify argument
+  {
+    return -1;
+  }
+
+  MFM_settings.slotModuleSettings[ sensorId - 1 ].enabled = status;
+
+  return (int32_t)MFM_settings.slotModuleSettings[ sensorId - 1 ].enabled;
+}
+
+/**
+ * @fn int getSensorType(int32_t)
+ * @brief override function getSensorType(), can be override in application code
+ *
+ * @param sensorId : 1 to /ref NR_OF_SLOTS
+ * @return
+ */
+const int32_t getSensorType(int32_t sensorId)
+{
+  assert_param(sensorId >= 1 && sensorId <= NR_OF_SLOTS );
+
+  if( sensorId < 1 || sensorId > NR_OF_SLOTS ) //Verify argument
+  {
+    return -1;
+  }
+
+  return (int32_t)MFM_settings.slotModuleSettings[ sensorId - 1 ].moduleType;
+}
+
+/**
+ * @brief override function getLoraInterval(), can be override in application code.
+ *
+ * @return Interval time in minutes
+ */
+const uint16_t getLoraInterval(void)
+{
+  return MFM_settings.intervalLora;
+}
+
+/**
+ * @brief override function setLoraInterval(), can be override in application code.
+ *
+ * @return Interval time in minutes
+ */
+const int32_t setLoraInterval(uint16_t interval)
+{
+  assert_param(interval >= PARA_LORA_INTERVAL_MIN && interval <= PARA_LORA_INTERVAL_MAX );
+
+  if( interval < PARA_LORA_INTERVAL_MIN || interval > PARA_LORA_INTERVAL_MAX ) //Verify argument
+  {
+    return -1;
+  }
+
+  MFM_settings.intervalLora = interval;
+
+  return (int32_t) MFM_settings.intervalLora;
+}
+
+/**
+ * @brief override function getMeetTijd(), can be override in application code.
+ *
+ * @param sensorId : 1 to /ref NR_OF_SLOTS
+ * @return Measure time in milliseconds
+ */
+const uint16_t getMeasureTime(int32_t sensorId)
+{
+  assert_param(sensorId >= 1 && sensorId <= NR_OF_SLOTS );
+
+  if( sensorId < 1 || sensorId > NR_OF_SLOTS ) //Verify argument
+  {
+    return -1;
+  }
+
+  return (int)MFM_settings.slotModuleSettings[ sensorId - 1 ].measureTime;
+}
+
+/**
+ * @brief override function getMeetTijd(), can be override in application code.
+ *
+ * @param sensorId : 1 to /ref NR_OF_SLOTS
+ * @return Measure time in milliseconds
+ */
+const int32_t setMeasureTime(int32_t sensorId, uint16_t measureTime )
+{
+  assert_param(sensorId >= 1 && sensorId <= NR_OF_SLOTS );
+  assert_param(measureTime >= PARA_MEASURE_TIME_MIN && measureTime <= PARA_MEASURE_TIME_MAX );
+
+
+  if( sensorId < 1 || sensorId > NR_OF_SLOTS ) //Verify argument
+  {
+    return -1;
+  }
+
+  if( measureTime < PARA_MEASURE_TIME_MIN || measureTime > PARA_MEASURE_TIME_MAX ) //Verify argument
+  {
+    return -2;
+  }
+
+  MFM_settings.slotModuleSettings[ sensorId - 1 ].measureTime = measureTime;
+
+  return (int32_t) MFM_settings.slotModuleSettings[ sensorId - 1 ].measureTime;
+}
+
+
+/**
+ * @fn const void setAlwaysOn(bool)
+ * @brief override function to set always on parameter
+ *
+ * @param state : true is enabled, false is disabled
+ */
+const void setAlwaysOn(bool state)
+{
+  MFM_settings.alwaysOnSupplyEnabled = state;
+}
+
+/**
+ * @fn const bool getAlwaysOn(void)
+ * @brief override function to get always on parameter
+ *
+ * @return state of always on, true = enabled, false = disabled
+ */
+const bool getAlwaysOn(void)
+{
+
+  return MFM_settings.alwaysOnSupplyEnabled;
+}
--- App/MFMconfiguration.h
+++ App/MFMconfiguration.h
@@ -9,6 +9,29 @@
 #ifndef MFMCONFIGURATION_H_
 #define MFMCONFIGURATION_H_
 
+#define NR_OF_SLOTS   6
 
+#define PARA_LORA_INTERVAL_MIN  5
+#define PARA_LORA_INTERVAL_MAX  1440
+
+#define PARA_MEASURE_TIME_MIN  0
+#define PARA_MEASURE_TIME_MAX  60000L
+
+typedef struct __attribute__((__packed__))
+{
+    uint16_t moduleType;      //received from sensor module
+    uint16_t measureTime;     //time between enable sensor and first measurement.
+    bool enabled;             //true = sensor slot is enabled, false = sensor slot is disabled
+    uint8_t reserved[11];     //reserved memory for future use
+}struct_sensorSlotSettings;
+
+typedef struct __attribute__((__packed__))
+{
+    uint16_t intervalLora;      //interval of lora sending
+    bool alwaysOnSupplyEnabled; //true = alwaysOnSupply is always on, false = alwaysOnSupply is off in sleep
+    struct_sensorSlotSettings slotModuleSettings[NR_OF_SLOTS];
+    uint8_t spare[154];         //reserved memory for future use
+    uint16_t crc;               //CRC for validate the data
+}struct_MFMSettings;
 
 #endif /* MFMCONFIGURATION_H_ */
