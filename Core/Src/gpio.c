4ea749103ec984e06244847ab300930b8ff9a634
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-09-27 14:03:30 +0200

    change: Make sure CS for SD is high at power. code change of previous commit 8e6d512d24e55d91f38071ffa9d9c80e46212d63

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -51,7 +51,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(SD_CS_GPIO_Port, SD_CS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(SD_CS_GPIO_Port, SD_CS_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : PA11 */
   GPIO_InitStruct.Pin = GPIO_PIN_11;
