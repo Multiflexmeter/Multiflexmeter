8a6891173402cf4e9c19103950daaa83816218b3
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-10-04 08:59:42 +0200

    change: add TCA9535 I2C driver, based on SLVC564 I/O Expander Software and Firmware Package (https://www.ti.com/tool/download/SLVC564)

---
+++ App/IO_Expander/TCA9535.c
@@ -0,0 +1,159 @@
+
+
+#include "main.h"
+#include "i2c.h"
+#include "TCA9535.h"
+
+
+enum I2C_State{
+  I2C_OPERATION_SUCCESSFUL =0,
+  I2C_OPERATION_FAIL = 1
+};
+
+extern unsigned char NACK;
+
+
+/**
+ * @fn int8_t I2C_Write(uint16_t, uint16_t, uint8_t, uint8_t*, uint8_t)
+ * @brief wrapper function to write a maximum of two bytes to a register address
+ *
+ * @param Size : number of databytes in data array for I2C write, must be 2 or smaller for this wrapper
+ * @param DevAddress : device address of I2C to write to
+ * @param registerAddress : register address to write to
+ * @param pData : pointer to data array
+ * @param offset : offset, not used, must be 0.
+ * @return result : negative value is error in arguments, 0 = successful, larger then 0 is HAL error.
+ */
+int8_t I2C_Write(uint8_t Size, uint16_t DevAddress, uint8_t registerAddress, uint8_t * pData, uint8_t offset )
+{
+  assert_param( Size <= 2 );
+  assert_param( offset != 0 );
+
+  UNUSED(offset);
+
+  if( Size > 2 )
+       return -1;
+
+  if( offset == 0 )
+    return -2;
+
+  uint8_t data[3];
+
+  data[0] = registerAddress;
+  data[1] = pData[1];
+  data[2] = pData[2];
+
+  return HAL_I2C_Master_Transmit(HI2C_TCA9535, DevAddress, data, sizeof(data), TCA9535_HAL_TIMEOUT);
+}
+
+/**
+ * @fn int8_t I2C_Read(uint8_t, uint16_t, uint8_t, uint8_t*)
+ * @brief wrapper function to read maximum of two bytes of a register address
+ *
+ * @param Size : number of databytes two be read from register address, must be 2 or smaller
+ * @param DevAddress : device address of I2C to write to
+ * @param registerAddress : register address to write to
+ * @param pData : pointer to data array for storing the readed data.
+ * @return result : negative value is error in arguments, 0 = successful, larger then 0 is HAL error.
+ */
+int8_t I2C_Read( uint8_t Size, uint16_t DevAddress, uint8_t registerAddress, uint8_t * pData )
+{
+  assert_param( Size <= 2 );
+
+  if( Size > 2 )
+    return -1;
+
+  return HAL_I2C_Mem_Read(HI2C_TCA9535, DevAddress, registerAddress, I2C_MEMADD_SIZE_8BIT, pData, Size, TCA9535_HAL_TIMEOUT);
+}
+
+// ****************************************************************************
+//! @fn          void TCA9535InitDefault(TCA9535Regs* Regs)
+//! @brief
+//!				 Initializes the confRegs structure which is
+//!              a local mirror of the TPL7200 config registers
+//!              additional TPL7200writeConfigRegisters(confRegs) function call
+//! 			 is needed in order to get the TPL7200 registers updated
+//!
+//! TODO 		 Implement I2C return (success/failure)
+// ****************************************************************************
+void TCA9535InitDefault(TCA9535Regs* Regs){
+	Regs->Output.all = 0xFFFF;
+	Regs->PolarityInversion.all = 0x0000;
+	Regs->Config.all = 0x0000;
+}
+
+
+// ****************************************************************************
+//! @fn          void TCA9535InitI2CReg(TCA9535Regs* Regs)
+//! @brief
+//!
+//! TODO 		 Implement I2C return (success/failure)
+// ****************************************************************************
+unsigned char TCA9535InitI2CReg(TCA9535Regs* Regs){
+	unsigned char return_Value = I2C_OPERATION_SUCCESSFUL;
+
+	I2C_Write(2, TCA9535_ADDRESS, TCA9535_CONFIG_REG0, (unsigned char*)&Regs->Config, 0);
+	I2C_Write(2, TCA9535_ADDRESS, TCA9535_OUTPUT_REG0, (unsigned char*)&Regs->Output, 0);
+	I2C_Write(2, TCA9535_ADDRESS, TCA9535_POLARITY_REG0, (unsigned char*)&Regs->PolarityInversion, 0);
+	if(NACK)
+	{
+		NACK = 0;
+
+		return_Value = I2C_OPERATION_FAIL;
+	}
+
+	return return_Value;
+}
+
+// ****************************************************************************
+//! @fn          void TCA9535ReadInputReg(TCA9535Regs* Regs)
+//! @brief
+//!
+//! TODO 		 Implement I2C return (success/failure)
+// ****************************************************************************
+void TCA9535ReadInputReg(TCA9535Regs* Regs){
+	I2C_Read(2,TCA9535_ADDRESS, TCA9535_INPUT_REG0, (unsigned char*)&Regs->Input);
+}
+
+
+// ****************************************************************************
+//! @fn          void TCA9535InitI2CReg(TCA9535Regs* Regs)
+//! @brief
+//!
+//! TODO 		 Implement I2C return (success/failure)
+// ****************************************************************************
+void TCA9535WriteReg(unsigned char address, unsigned char regaddress, unsigned short regVal)
+{
+	//I2C_Write(2, address, regaddress, (regVal), 0);
+	if(NACK)
+	{
+
+
+	}
+
+}
+
+//unsigned char TCA9535ReadReg(unsigned char address, unsigned char regaddress)
+//{
+//	return (unsigned char) I2C_Read_Byte(address,regaddress);
+//}
+
+void TCA9535WriteConfig(TCA9535Regs * Regs)
+{
+	I2C_Write(2, TCA9535_ADDRESS, TCA9535_CONFIG_REG0, (unsigned char*)&Regs->Config, 0);
+}
+
+void TCA9535WriteOutput(TCA9535Regs * Regs)
+{
+
+	I2C_Write(2, TCA9535_ADDRESS, TCA9535_OUTPUT_REG0, (unsigned char*)&Regs->Output, 0);
+}
+
+void TCA9535WritePolarity(TCA9535Regs * Regs)
+{
+
+	I2C_Write(2, TCA9535_ADDRESS, TCA9535_POLARITY_REG0, (unsigned char*)&Regs->PolarityInversion, 0);
+}
+
+
+
---
+++ App/IO_Expander/TCA9535.h
@@ -0,0 +1,136 @@
+
+#ifndef TCA9535_H_
+#define TCA9535_H_
+
+extern I2C_HandleTypeDef hi2c2;
+#define HI2C_TCA9535   &hi2c2
+
+#define TCA9535_HAL_TIMEOUT ( 100 ) //ms
+
+
+/************************** I2C Address ***************************************/
+#define TCA9535_ADDRESS		0x26 		// I2C Address 0100 00 + ADDR + R/W
+											// ADDR tied to P2.2 of LaunchPad
+/************************** I2C Registers *************************************/
+#define TCA9535_INPUT_REG0 		0x00		// Input status register
+#define TCA9535_INPUT_REG1 		0x01		// Input status register
+#define TCA9535_OUTPUT_REG0		0x02		// Output register to change state of output BIT set to 1, output set HIGH
+#define TCA9535_OUTPUT_REG1		0x03		// Output register to change state of output BIT set to 1, output set HIGH
+#define TCA9535_POLARITY_REG0 	0x04		// Polarity inversion register. BIT '1' inverts input polarity of register 0x00
+#define TCA9535_POLARITY_REG1 	0x05		// Polarity inversion register. BIT '1' inverts input polarity of register 0x00
+#define TCA9535_CONFIG_REG0		0x06		// Configuration register. BIT = '1' sets port to input BIT = '0' sets port to output
+#define TCA9535_CONFIG_REG1		0x07		// Configuration register. BIT = '1' sets port to input BIT = '0' sets port to output
+
+
+struct TCA9535_sBit{
+       unsigned char B0:1;
+       unsigned char B1:1;
+       unsigned char B2:1;
+       unsigned char B3:1;
+       unsigned char B4:1;
+       unsigned char B5:1;
+       unsigned char B6:1;
+       unsigned char B7:1;
+};
+
+union TCA9535_uInputPort{
+	unsigned char all;
+	struct TCA9535_sBit bit;
+};
+
+struct TCA9535_sInput{
+	union TCA9535_uInputPort P0;
+	union TCA9535_uInputPort P1;
+};
+
+
+union TCA9535_uInput{
+	unsigned short all;
+	struct TCA9535_sInput Port;
+};
+
+
+
+union TCA9535_uBitAll{
+	unsigned char 		all;
+	struct TCA9535_sBit	bit;
+};
+/*
+union TCA9535_uOutputP1{
+	unsigned char 		all;
+	struct TCA9535_sBit	bit;
+};*/
+
+struct TCA9535_sOutput{
+	union TCA9535_uBitAll	P0;
+	union TCA9535_uBitAll	P1;
+};
+
+union TCA9535_uOutput{
+	unsigned short	all;
+	struct TCA9535_sOutput	Port;
+};
+/*
+union TCA9535_uPolarityInversionP0{
+	unsigned char all;
+	struct TCA9535_sBit bit;
+};
+
+union  TCA9535_uPolarityInversionP1{
+	unsigned char	all;
+	struct TCA9535_sBit bit;
+};
+*/
+struct TCA9535_sPolarityInversion{
+	union TCA9535_uBitAll P0;
+	union TCA9535_uBitAll P1;
+};
+
+union TCA9535_uPolarityInversion{
+	unsigned short all;
+	struct TCA9535_sPolarityInversion Port;
+};
+/*
+union TCA9535_uConfigP0{
+	unsigned char all;
+	struct TCA9535_sBit bit;
+};
+
+union  TCA9535_uConfigP1{
+	unsigned char	all;
+	struct TCA9535_sBit bit;
+};*/
+
+struct TCA9535_sConfig{
+	union TCA9535_uBitAll P0;
+	union TCA9535_uBitAll P1;
+};
+
+union TCA9535_uConfig{
+	unsigned short all;
+	struct TCA9535_sConfig Port;
+};
+
+struct sTCA9535Regs{
+	union TCA9535_uInput 				Input;
+	union TCA9535_uOutput 				Output;
+	union TCA9535_uPolarityInversion 	PolarityInversion;
+	union TCA9535_uConfig				Config;
+};
+
+typedef struct {
+	union TCA9535_uInput 				Input;
+	union TCA9535_uOutput				Output;
+	union TCA9535_uPolarityInversion	PolarityInversion;
+	union TCA9535_uConfig				Config;
+} TCA9535Regs;
+
+void TCA9535WriteConfig(TCA9535Regs * Regs);
+void TCA9535WriteOutput(TCA9535Regs * Regs);
+void TCA9535WritePolarity(TCA9535Regs * Regs);
+
+void TCA9535InitDefault(TCA9535Regs* Regs);
+unsigned char TCA9535InitI2CReg(TCA9535Regs* Regs);
+void TCA9535ReadInputReg(TCA9535Regs* Regs);
+
+#endif
