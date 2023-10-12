20579b5afe0306c798c26f4e3b68a4a503d496cc
Danny Kerstens <Danny.kerstens@dekimo.com>
2023-10-12 11:15:23 +0200

    Docs: added doxygen descriptions to functions

--- App/I2CMaster/I2C_Master.c
+++ App/I2CMaster/I2C_Master.c
@@ -15,7 +15,8 @@ extern I2C_HandleTypeDef hi2c2;
  * @param slaveAddress The i2c slave address of the sensor module
  * @param regAddress The address of the register that needs to be read
  * @param data Pointer to a buffer to store the result in.
- * @return Return I2C_OK when the function is successful. Returns a specific error based on the type of fault.
+ * @return Return I2C_OK when the function is successful.
+ *         Else returns a specific error based on the type of fault.
  */
 tENUM_SensorError sensorMasterRead(uint8_t slaveAddress, uint8_t regAddress, uint8_t *data)
 {
@@ -49,7 +50,8 @@ tENUM_SensorError sensorMasterRead(uint8_t slaveAddress, uint8_t regAddress, uin
  * @param slaveAddress The i2c slave address of the sensor module
  * @param regAddress The address of the register to write
  * @param data The data to write
- * @return Return I2C_OK when the function is successful. Returns a specific error based on the type of fault.
+ * @return Return I2C_OK when the function is successful.
+ *         Else returns a specific error based on the type of fault.
  */
 tENUM_SensorError sensorMasterWrite(uint8_t slaveAddress, uint8_t regAddress, uint8_t *data)
 {
@@ -82,7 +84,7 @@ tENUM_SensorError sensorMasterWrite(uint8_t slaveAddress, uint8_t regAddress, ui
 /**
  * @brief Get the firmware version of the sensor
  *
- * @param address is the sensor address
+ * @param address The sensor address
  */
 void sensorFirmwareVersion(SensorAddress address)
 {
@@ -93,7 +95,7 @@ void sensorFirmwareVersion(SensorAddress address)
 /**
  * @brief Get the protocol version of the sensor
  *
- * @param address is the sensor address
+ * @param address The sensor address
  * @return The protocol version
  */
 uint8_t sensorProtocolVersion(SensorAddress address)
@@ -103,6 +105,12 @@ uint8_t sensorProtocolVersion(SensorAddress address)
   return protocolVersion;
 }
 
+/**
+ * @brief Read the type of sensor connected
+ *
+ * @param address The sensor address
+ * @return The sensor type
+ */
 uint16_t sensorReadType(SensorAddress address)
 {
   uint8_t sensorType[2];
@@ -110,12 +118,23 @@ uint16_t sensorReadType(SensorAddress address)
   return sensorType[0] + (sensorType[1]<<8);
 }
 
+/**
+ * @brief Start the measurement
+ *
+ * @param address The sensor address
+ */
 void sensorStartMeasurement(SensorAddress address)
 {
   uint8_t startCommand = 0x01;
   sensorMasterWrite(address, REG_MEAS_START, &startCommand);
 }
 
+/**
+ * @brief Read the measurement status
+ *
+ * @param address The sensor address
+ * @return The measurement status
+ */
 uint8_t sensorMeasurementStatus(SensorAddress address)
 {
   uint8_t sensorStatus;
@@ -123,12 +142,24 @@ uint8_t sensorMeasurementStatus(SensorAddress address)
   return sensorStatus;
 }
 
+/**
+ * @brief Write the time to wait after applying the power to start measuring
+ *
+ * @param address The sensor address
+ * @param setupTime The time to wait after applying power
+ */
 void sensorWriteSetupTime(SensorAddress address, uint16_t setupTime)
 {
   uint8_t setupTimeBuffer[2] = {(setupTime), setupTime>>8};
   sensorMasterWrite(address, REG_MEAS_TIME, setupTimeBuffer);
 }
 
+/**
+ * @brief Read the setup time
+ *
+ * @param address The sensor address
+ * @return The setup time
+ */
 uint16_t sensorReadSetupTime(SensorAddress address)
 {
   uint8_t setupTime[2];
@@ -141,26 +172,72 @@ void sensorReadMeasurement(SensorAddress address)
   //TODO implement sensor type function
 }
 
-void sensorAmount(SensorAddress address)
+/**
+ * @brief Read the amount of sensors connected
+ *
+ * @param address The sensor address
+ * @return The amount of sensors connected
+ */
+uint8_t sensorReadAmount(SensorAddress address)
 {
-  //TODO implement sensor type function
+  uint8_t sensorAmount;
+  sensorMasterRead(address, REG_SENSOR_AMOUNT, &sensorAmount);
+  return sensorAmount;
 }
 
-void sensorSelect(SensorAddress address)
+/**
+ * @brief Read which sensor is selected
+ *
+ * @param address The sensor address
+ * @return The selected sensor
+ */
+uint8_t sensorReadSelection(SensorAddress address)
 {
-  //TODO implement sensor type function
+  uint8_t sensor;
+  sensorMasterRead(address, REG_SENSOR_SELECTED, &sensor);
+  return sensor;
 }
 
-void sensorSampleType(SensorAddress address)
+/**
+ * @brief Select which sensor to sample from
+ *
+ * @param address The sensor address
+ * @param sensor The sensor to select
+ */
+void sensorWriteSelection(SensorAddress address, uint8_t sensor)
 {
-  //TODO implement sensor type function
+  sensorMasterWrite(address, REG_SENSOR_SELECTED, &sensor);
+}
+
+/**
+ * @brief Set how to calculate the samples
+ *
+ * @param address The sensor address
+ * @param sampleType The type of sample
+ */
+void sensorWriteSampleType(SensorAddress address, uint8_t sampleType)
+{
+  sensorMasterWrite(address, REG_MEAS_TYPE, &sampleType);
+}
+
+/**
+ * @brief Read the sample type
+ *
+ * @param address The sensor address
+ * @return The sample type
+ */
+uint8_t sensorReadSampleType(SensorAddress address)
+{
+  uint8_t sampleType;
+  sensorMasterRead(address, REG_MEAS_TYPE, &sampleType);
+  return sampleType;
 }
 
 /**
  * @brief Read the amount of samples the sensor card has to take for a measurement.
  *
- * @param address is the address of the sensor
- * @return the amount of samples
+ * @param address The sensor address
+ * @return The amount of samples
  */
 uint8_t sensorReadSamples(SensorAddress address)
 {
@@ -169,6 +246,12 @@ uint8_t sensorReadSamples(SensorAddress address)
   return samples;
 }
 
+/**
+ * @brief Set the amount of samples to take for a measurement
+ *
+ * @param address The sensor address
+ * @param samples The amount of samples to take
+ */
 void sensorSetSamples(SensorAddress address, uint8_t samples)
 {
   sensorMasterWrite(address, REG_MEAS_SAMPLES, &samples);
