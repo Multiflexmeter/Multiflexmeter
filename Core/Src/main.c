972e119c3a935f15ed708cd5c0cc516c3ea496f4
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-10-17 11:41:51 +0200

    change: disable other CLOCK inits (executing failed on current target).

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -65,8 +65,8 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
-#define CHECK_ON_TCXO_32MHZ  //enable for evaluation board Nucleo-WL55JC1 //todo disable at final target.
-#define CHECK_ON_CRYSTAL     //enable for Dekimo board
+//#define CHECK_ON_TCXO_32MHZ  //enable for evaluation board Nucleo-WL55JC1 //todo disable at final target.
+//#define CHECK_ON_CRYSTAL     //enable for Dekimo board
 //#define CHECK_ON_TCXO        //enable for Dekimo board
 
 //#define ERASE_VIRTUAL_EEPROM  //enable for erasing virtual EEPROM
