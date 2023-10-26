13666434c423a3730add450a446a8d28d5e4bb9e
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-10-26 15:30:10 +0200

    change: move I2C init before LoraWan Init. Code change of previous commit.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -179,12 +179,12 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SPI1_Init();
+  MX_I2C1_Init();
+  MX_I2C2_Init();
   MX_LoRaWAN_Init();
   MX_USART1_UART_Init();
-  MX_I2C1_Init();
   MX_CRC_Init();
   MX_FATFS_Init();
-  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
   uartInit_Config();
   resultInitDataflash = init_dataflash();
