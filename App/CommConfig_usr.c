88ffaf2834783ecfc3810685b4bc0194319db3d7
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2023-11-21 16:17:54 +0100

    change: implement of vBusSupply measurement

--- App/CommConfig_usr.c
+++ App/CommConfig_usr.c
@@ -12,7 +12,20 @@
   */
 
 #include "adc_if.h"
+#include "common/common.h"
 #include "CommConfig_usr.h"
+
+/**
+ * @fn int32_t SYS_GetAdc(int)
+ * @brief weak function SYS_GetAdc(), can be override in application code
+ *
+ * @param sensorId
+ * @return
+ */
+__weak const uint32_t SYS_GetAdc(int channel)
+{
+  return 0;
+}
 
 /**
  * @brief override function for getBatterijSupply()
@@ -25,5 +38,14 @@ const uint16_t getBatterijSupply(void)
   return SYS_GetBatteryLevel();
 }
 
-
+/**
+ * @fn const uint16_t getVbusSupply(void)
+ * @brief override function for getVbusSupply
+ *
+ * @return vbus supply in mV
+ */
+const uint16_t getVbusSupply(void)
+{
+  return (uint16_t)SYS_GetAdc(CHANNEL_4_ADC);
+}
 
