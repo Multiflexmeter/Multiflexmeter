4887f16a2eac16438888a6e3972a443b52c3cdf6
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2024-02-14 15:52:21 +0100

    change: softwareVersion to 0.1.01

--- App/common/softwareVersion.c
+++ App/common/softwareVersion.c
@@ -13,7 +13,7 @@
 #include "softwareVersion.h"
 
 
-#define MFM_SOFTWARE_VERSION  "0.1.00"
+#define MFM_SOFTWARE_VERSION  "0.1.01"
 #define MFM_CONFIG_PROTOCOL_VERSION  "0"
 #define MFM_SENSOR_PROTOCOL_VERSION  "0"
 
