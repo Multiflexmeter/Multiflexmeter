33cdcd305d9765c9cb5d3c49eae72e6146a0127f
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2024-04-08 15:55:47 +0200

    Change: print some debug info.

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -21,7 +21,7 @@
 #include "i2c.h"
 
 /* USER CODE BEGIN 0 */
-
+#include "sys_app.h"
 /* USER CODE END 0 */
 
 I2C_HandleTypeDef hi2c1;
@@ -244,12 +244,14 @@ void HAL_I2C_checkError(void)
 {
   if (hi2c1.ErrorCode)
   {
+    APP_LOG(TS_OFF, VLEVEL_H, "I2C ch1 error: %u\r\n", hi2c1.ErrorCode );
     HAL_I2C_MspDeInit(&hi2c1);
     HAL_I2C_MspInit(&hi2c1);
   }
 
   if (hi2c2.ErrorCode)
   {
+    APP_LOG(TS_OFF, VLEVEL_H, "I2C ch2 error: %u\r\n", hi2c1.ErrorCode );
     HAL_I2C_MspDeInit(&hi2c2);
     HAL_I2C_MspInit(&hi2c2);
   }
